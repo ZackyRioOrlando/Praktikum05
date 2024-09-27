8fcdc48ae4b3d03849a8dbbb5d30129f65aa2d57
Zacky Rio Orlando <zackyrio555@gmail.com>
2024-09-27 09:20:00 +0700

    Praktikum05

---
+++ ModifikasiPemilihanBilangan27.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ModifikasiPemilihanBilangan27 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+        
+        System.out.println("Angka " + angka + " termasuk bilangan " + hasil);
+    }
+}
